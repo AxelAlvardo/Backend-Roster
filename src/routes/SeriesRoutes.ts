10abb79a872011eed2d1d3a33deb790c689f6436
AxelAlvardo <00216022@uca.edu.sv>
2024-08-09 22:44:34 -0600

    Add : Serie Create

--- src/routes/SeriesRoutes.ts
+++ src/routes/SeriesRoutes.ts
@@ -3,6 +3,7 @@ import { SeriesController } from "../controllers/SeriesController";
 
 const router = Router();
 
+router.post('/', SeriesController.createSerie);
 router.get('/', SeriesController.getAllSeries);
 
 export default router;
