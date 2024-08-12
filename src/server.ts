365be31391bb6fe065772af6efd0eeff8e18f920
AxelAlvardo <00216022@uca.edu.sv>
2024-08-11 23:21:47 -0600

    Add : Documentation and URL

--- src/server.ts
+++ src/server.ts
@@ -7,13 +7,18 @@ import characterRoutes from './routes/CharacterRoute'
 import moviesRoutes from './routes/MoviesRouter'
 import seriesRoutes from './routes/SeriesRoutes'
 import booksRoutes from './routes/BooksRoute'
+import swaggerUI from 'swagger-ui-express'
+import swaggerSpec from './config/swagger';
 
 dotenv.config();
 
 connectDB();
 
 const server = express();
-server.use(cors(corsConfig));
+server.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}));
 server.use(express.json());
 
 //Routes
@@ -21,5 +26,8 @@ server.use('/api/characters', characterRoutes);
 server.use('/api/movies', moviesRoutes)
 server.use('/api/series', seriesRoutes)
 server.use('/api/books', booksRoutes)
+
+//DOCS
+server.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 
 export default server;
